f433a12624b99d34b20bb7afe90479290f369b7f
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2018-05-11 16:05:48 +0200

    fix test after merge to rapidxml

--- test/exml_query_tests.erl
+++ test/exml_query_tests.erl
@@ -63,12 +63,12 @@ no_element_with_ns_query_test() ->
 
 element_with_attr_query_test() ->
     ?assertEqual(xml(<<"<received xmlns='urn:xmpp:chat-markers:0'
-                                id='0047ee62-9418-4ef8-abd8-0d08e4140b72'/>)">>),
+                                id='0047ee62-9418-4ef8-abd8-0d08e4140b72'/>">>),
                  exml_query:subelement_with_attr(chat_marker(), <<"xmlns">>,
                                                  <<"urn:xmpp:chat-markers:0">>)),
 
     ?assertEqual(xml(<<"<received xmlns='urn:xmpp:chat-markers:0'
-                                id='0047ee62-9418-4ef8-abd8-0d08e4140b72'/>)">>),
+                                id='0047ee62-9418-4ef8-abd8-0d08e4140b72'/>">>),
                  exml_query:path(chat_marker(), [{element_with_attr, <<"xmlns">>,
                                                   <<"urn:xmpp:chat-markers:0">>}])).
 
